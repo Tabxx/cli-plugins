97e8b0fa23ad971f8c2460fba250ea7316167f9c
Tabxx <1023106595@qq.com>
2020-06-14 13:48:06 +0800

    123

--- package/git.js
+++ package/git.js
@@ -2,7 +2,7 @@ const shell = require('shelljs')
 const original = JSON.parse(process.env.npm_config_argv).original
 const commit = original[3]
 
-console.log('s111ss')
+console.log('s11122ss')
 
 shell.exec('git status')
 shell.exec('git add .')
